3f1bf8830631a79c99764eab6fc9c6bcc70f6ff3
Love Westlund Gotby <lovewestlundgotby@gmail.com>
2019-03-21 08:42:40 +0100

    Start searching in my home directory

--- rofi_scripts/rofi_locate.sh
+++ rofi_scripts/rofi_locate.sh
@@ -3,6 +3,6 @@
 # Usage: bind key to run the script. It will open a rofi prompt that will search
 # through the chosen directory dir.
 
-dir="home"
+dir="/home/love"
 
 xdg-open "$(locate $dir | rofi -threads 0 -width 1920 -padding 300 -dmenu -i -p "locate")"
